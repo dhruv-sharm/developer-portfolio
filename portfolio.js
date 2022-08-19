4ba3ee40abfb3da0e06c187383c2ede79f166797
Dhruv Sharma <72140293+dhruv-sharm@users.noreply.github.com>
2022-08-19 18:13:33 +0530

    first commit

--- portfolio.js
+++ portfolio.js
@@ -1,26 +1,26 @@
 import emoji from "react-easy-emoji";
 
 export const greetings = {
-	name: "Hanzla Tauqeer",
-	title: "Hi all, I'm Hanzla",
+	name: "Dhruv  Sharma",
+	title: "Hi all, I'm Dhruv..",
 	description:
-		"I'm passionate Full Stack web developer having an experience of web applications with Python, Django, React.js, Next.js and Blockchain development on Ethereum, Solidity, Web3.js, Moralis, and Brownie Framework.",
+		"I'm passionate Full Stack web developer having an experience of web applications with MongoDB, Express, React, Node, Bootstrap and Tailwind CSS-Framework",
 	resumeLink:
-		"https://drive.google.com/file/d/1WD21Y1fzGmdKY6R8lrK01X4YH9RBUxOa/view?usp=sharing",
+	"https://drive.google.com/file/d/1zteD_LOFxpv91X5eE69-LlmG83FIunvw/view?usp=sharing",
 };
 
 export const openSource = {
-	githubUserName: "1hanzla100",
+	githubUserName: "dhruv-sharm",
 };
 
 export const contact = {};
 
 export const socialLinks = {
-	facebook: "https://www.facebook.com/1hanzla100",
-	instagram: "https://www.instagram.com/__hanzla100",
-	twitter: "https://twitter.com/1hanzla100",
-	github: "https://github.com/1hanzla100",
-	linkedin: "https://www.linkedin.com/in/1hanzla100/",
+	facebook: "https://www.facebook.com/dhruv.sharmx",
+	instagram: "https://www.instagram.com/dhruvv095",
+	twitter: "https://twitter.com/dhruv_sharmx",
+	github: "https://github.com/dhruv-sharm",
+	linkedin: "https://www.linkedin.com/in/dhruv-sharma170",
 };
 
 export const skillsSection = {
@@ -35,9 +35,9 @@ export const skillsSection = {
 				emoji(
 					"⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js"
 				),
-				emoji("⚡ Building responsive static websites using Next.js"),
+				emoji("⚡ Building responsive static websites using Html, CSS & JavaScript"),
 				emoji(
-					"⚡ Building RESTful APIs in Django & Django REST Framework"
+					"⚡ Building RESTful APIs using Scratch"
 				),
 			],
 			softwareSkills: [
@@ -58,23 +58,6 @@ export const skillsSection = {
 					fontAwesomeClassname: "vscode-icons:file-type-reactjs",
 				},
 				{
-					skillName: "Nextjs",
-					fontAwesomeClassname: "vscode-icons:file-type-light-next",
-				},
-				{
-					skillName: "Python",
-					fontAwesomeClassname: "logos:python",
-				},
-				{
-					skillName: "Django",
-					fontAwesomeClassname: "vscode-icons:file-type-django",
-				},
-
-				{
-					skillName: "Redux",
-					fontAwesomeClassname: "logos:redux",
-				},
-				{
 					skillName: "NPM",
 					fontAwesomeClassname: "logos:npm-icon",
 				},
@@ -85,35 +68,44 @@ export const skillsSection = {
 			],
 		},
 		{
-			title: "Cloud Infra-Architecture",
-			lottieAnimationFile: "/lottie/skills/cloudinfra.json", // Path of Lottie Animation JSON File
+			title: "Database",
+			lottieAnimationFile: "/lottie/skills/S67eoQh5Id.json", // Path of Lottie Animation JSON File
 			skills: [
-				emoji("⚡ Experience of working on multiple cloud platforms"),
-				emoji(
-					"⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases"
-				),
-				emoji(
-					"⚡ Building CI/CD pipelines for automated testing & deployment using Github Actions"
-				),
+				emoji("⚡ using both database SQL and Non-SQL as per requirment"),
+				emoji("⚡ using My-sql for SQL database"),
+				emoji("⚡ using MongoDB for Non-SQL database"),
 			],
 			softwareSkills: [
 				// ? Check README To get icon details
 				{
-					skillName: "AWS",
-					fontAwesomeClassname: "logos:aws",
+					skillName: "MongoDB",
+					fontAwesomeClassname: "logos:mongodb-icon",
 				},
 				{
-					skillName: "Azure",
-					fontAwesomeClassname: "logos:microsoft-azure",
+					skillName: "SQL",
+					fontAwesomeClassname: "bi:filetype-sql",
 				},
+
+
+			],
+		},
+		
+		{
+			title: "Hosting & Management",
+			lottieAnimationFile: "/lottie/skills/ethereum.json", // Path of Lottie Animation JSON File
+			skills: [
+				emoji(
+					"⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases"
+				),emoji("⚡ other TECH skill"),
+			],
+			softwareSkills: [
+				
+					
 				{
 					skillName: "Heroku",
 					fontAwesomeClassname: "logos:heroku-icon",
 				},
-				{
-					skillName: "PostgreSQL",
-					fontAwesomeClassname: "logos:postgresql",
-				},
+				
 				{
 					skillName: "Github",
 					fontAwesomeClassname: "akar-icons:github-fill",
@@ -126,64 +118,10 @@ export const skillsSection = {
 					skillName: "Github Actions",
 					fontAwesomeClassname: "logos:github-actions",
 				},
-				{
-					skillName: "Cloudinary",
-					fontAwesomeClassname: "logos:cloudinary",
-				},
-				{
-					skillName: "Nginx",
-					fontAwesomeClassname: "logos:nginx",
-				},
-				{
-					skillName: "Sentry",
-					fontAwesomeClassname: "logos:sentry-icon",
-				},
-			],
+					
+			]		
 		},
-		{
-			title: "Blockchain",
-			lottieAnimationFile: "/lottie/skills/ethereum.json", // Path of Lottie Animation JSON File
-			skills: [
-				emoji(
-					"⚡ Experience in developing Smart Contract using Solidity & Ethereum"
-				),
-				emoji(
-					"⚡ Building Scripts for automated testing & deployment of Smart Contracts using Brownie & Infura"
-				),
-				emoji(
-					"⚡ Experience of using Openzeppelin Smart Contract Standards & Chainlink Oracles"
-				),
-				emoji(
-					"⚡ Developing NFT Smart Contracts using ERC-721 Token Standard"
-				),
-				emoji(
-					"⚡ Building Dapps with React.js & Solidity using Web3.js, Moralis & IPFS"
-				),
-			],
-			softwareSkills: [
-				{
-					skillName: "Ethereum",
-					fontAwesomeClassname: "logos:ethereum",
-				},
-				{
-					skillName: "Solidity",
-					fontAwesomeClassname: "logos:solidity",
-				},
-				{
-					skillName: "Web3js",
-					fontAwesomeClassname: "logos:web3js",
-				},
-				{
-					skillName: "Metamask",
-					fontAwesomeClassname: "logos:metamask-icon",
-				},
-				{
-					skillName: "Ganache",
-					fontAwesomeClassname: "logos:ganache-icon",
-				},
-			],
-		},
-	],
+	]
 };
 
 export const SkillBars = [
@@ -197,91 +135,52 @@ export const SkillBars = [
 	},
 	{
 		Stack: "Programming",
-		progressPercentage: "60",
+		progressPercentage: "80",
 	},
 ];
 
 export const educationInfo = [
 	{
-		schoolName: "Harvard University",
-		subHeader: "Master of Science in Computer Science",
-		duration: "September 2017 - April 2019",
-		desc: "Participated in the research of XXX and published 3 papers.",
-		grade: "Grade A",
+		schoolName: "Govt. Polytechnic ",
+		subHeader: "3 Years Diploma in Computer Science",
+		duration: "September 2019 - April 2012",
+		desc: "Learned some Skill to write code in :",
+		grade: "8.7 CGPA",
 		descBullets: [
-			"Lorem ipsum dolor sit amet, consectetur adipdfgiscing elit",
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+			"C & C++ ",
+			"Web Development",
+			"Software Development & Debugging",
+			
 		],
 	},
 ];
 
 export const experience = [
 	{
-		role: "Frontend Developer",
-		company: "Duseca Software",
-		companylogo: "/img/icons/common/dusecaSoftware.jpg",
-		date: "Apr 2022 – Jun 2022",
-		desc: "I worked as frontend web developer to design web based admin dashboards for mulitple apps using Django, Bootstrap, jQuery, Chart.js and some other libraries.",
-	},
-	{
-		role: "API Engineer",
-		company: "Duseca Software",
-		companylogo: "/img/icons/common/dusecaSoftware.jpg",
-		date: "Jan 2022 – Mar 2022",
-		desc: "I worked as API Engineer on a Kale App. Kale is a community platform for the creation of social reviews based on verified transactions. I built Fully Optimized and High Performance RESTful APIs using Django, DjangoRESTFramework, PostgresSQL, JWT Authentication, AWS, Integrated Plaid APIs for Fetching Bank Transactions, Integrated Sentry for monitoring Django Server and deployed the API on Heroku Server.",
-		// descBullets: [
-		// 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
-		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-		// ],
-	},
-	{
-		role: "Full Stack Developer",
-		company: "Bleed-AI",
-		companylogo: "/img/icons/common/bleedAI.jpg",
-		date: "Sept 2021 - Oct 2021",
-		desc: "Worked as django web developer to create a website in which I integrated computer vision AI model (built by the team of BleedAI) to process YouTube URLs and show Real Time processing on Website using Django Channels WebSockets , threading for running multiple instances of AI Model, jQuery for UI rendering and deployed it on Heroku server.",
-	},
-	{
-		role: "Backend Developer",
-		company: "Wapidu",
-		companylogo: "/img/icons/common/wapidu.jpg",
-		date: "Sept 2021",
-		desc: "Worked as a Django Developer to integrate Stripe payment gateway in wapidu.com and Created REST APIs using Django REST Framework to integrate Django Stripe backend to Vue.js Frontend and Deployed it on Azure based Docker container registry.",
+		role:"Full Stack Developer-(MERN)",
+		companylogo: "/img/icons/common/fullstack-logo.png",
+		desc: "I worked as Full Stack web developer to design web based admin dashboards for mulitple apps and backends using  Bootstrap, jQuery, React.js, Node, Express and some other libraries & framework, created REST APIs using scratch,  deployed it on Heroku server .",
+		
 	},
 ];
 
 export const projects = [
-	{
-		name: "developer-portfolio",
-		desc: "Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.",
-		github: "https://github.com/1hanzla100/developer-portfolio",
-		link: "https://developer-portfolio-1hanzla100.vercel.app/",
-	},
-	{
-		name: "AtlasMart",
-		desc: "With Atlas Mart, it’s easy to find the products with the best price and ship them straight to your door.",
-		github: "https://github.com/1hanzla100/Django-React-Marketplace",
-	},
-	{
-		name: "Technota (Forum)",
-		desc: "Get hands-on experience in technical skills with Technota",
-		github: "https://github.com/1hanzla100/django-react-forum",
-	},
-	{
-		name: "Shopaza (Ecommerce)",
-		desc: "Now your reach to all your favorite entertainment and luxury items is just one touch apart",
-		github: "https://github.com/1hanzla100/Django-ecommerce",
-	},
+	// {
+	// 	name: "developer-portfolio",
+	// 	desc: "Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.",
+	// 	github: "https://github.com/1hanzla100/developer-portfolio",
+	// 	link: "https://developer-portfolio-1hanzla100.vercel.app/",
+	// },
 ];
 
 export const feedbacks = [
 	{
-		name: "John Smith",
+		name: "Lorem-11",
 		feedback:
 			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.",
 	},
 	{
-		name: "John Smith",
+		name: "Lorem-12",
 		feedback:
 			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.",
 	},
@@ -289,19 +188,21 @@ export const feedbacks = [
 
 // See object prototype on SEO.jsx page
 export const seoData = {
-	title: "Hanzla Tauqeer",
+	title: "Dhruv Sharma",
 	description:
-		"A passionate Full Stack Web Developer and Blockchain Developer.",
-	author: "Hanzla Tauqeer",
+		"A passionate Full Stack Web Developer.",
+	author: "Dhruv Sharma",
 	image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
 	url: "https://developer-portfolio-1hanzla100.vercel.app",
 	keywords: [
-		"Hanzla",
-		"Hanzla Tauqeer",
-		"@1hanzla100",
-		"1hanzla100",
+		"Dhruv",
+		"Dhruv Sharma",
+		"@dhruv.sharmx",
+		"dhruv.sharmx",
 		"Portfolio",
-		"Hanzla Portfolio ",
-		"Hanzla Tauqeer Portfolio",
+		"Dhruv Portfolio ",
+		"Dhruv Sharma Portfolio",
+		"dhruv_sharmx",
+		"web developer"
 	],
 }
